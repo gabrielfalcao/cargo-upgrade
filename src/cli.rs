f0ad3a165152fa9f8c9a7149e24b4fb1b2003ed8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-23 22:13:19 +0000

    fix execname

--- src/cli.rs
+++ src/cli.rs
@@ -25,7 +25,7 @@ pub trait ParserDispatcher<E: std::error::Error>: clap::Parser {
         let args = iocore::env::args();
         let execname = iocore::Path::new(&args[0]).name();
         dbg!(&execname, &args);
-        let is_cargo = execname.ends_with("cargo") || execname.ends_with("cargo-craft");
+        let is_cargo = execname.ends_with("cargo") || execname.ends_with("cargo-upgrade");
         let args = if is_cargo { args[1..].to_vec() } else { args.to_vec() };
         (args, is_cargo)
     }
